6c41775681a654e120094079c0691e0b613a2e65
biswajitmailid <biswajitmailid@gmail.com>
2022-06-18 17:50:26 +0000

    Configure Cors and necessary middlewares.

--- readme.md
+++ readme.md
@@ -3,15 +3,21 @@
 ### Setting Up The Project (Commands)
 
 ```
-npm init
-npm install typescript tslint --save-dev
-tsc --init
-npm i @types/express @types/node --save-dev
+> npm init
+> npm install typescript tslint --save-dev
+> tsc --init
+> npm i @types/express @types/node --save-dev
 ```
 
 ### Creating The Server
 
 ```
-npm i inversify inversify-express-utils reflect-metadata --save
-npm i dotenv --save
+> npm i inversify inversify-express-utils reflect-metadata --save
+> npm i dotenv --save
+```
+
+### Configuring The Server
+
+```
+> npm install cors
 ```
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,30 @@
-import "reflect-metadata";
+import bodyParser from 'body-parser';
 import express from 'express';
 import { InversifyExpressServer } from "inversify-express-utils";
 import { GetIocContainer } from './common/iocRegister';
 
+var cors = require('cors');
 class Server {
     public start = (port: number) => {
         const container = GetIocContainer();
         const expressApp = express();
         let server = new InversifyExpressServer(container, null, { rootPath: "/api" }, expressApp);
+
+        server.setConfig((app) => {
+            app.use(cors());
+            app.use(bodyParser.urlencoded({
+                extended: true
+            }));
+            app.use(bodyParser.json());
+        });
+
+        server.setErrorConfig((app) => {
+            app.use((err: any, req: any, res: any, next: any) => {
+                console.error(err.stack);
+                res.status(500).send(`An Error Occurred!`);
+            })
+        });
+
         const app = server.build();
         let serve: any = app.listen(port, () =>
             console.log(`App running on ${serve.address().port}`));
